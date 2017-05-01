85362ba52865259f9640c677ec81e16f6eb4af20
ramakrishna0008 <ramakrishna0008@gmail.com>
2017-05-01 23:08:31 +0530

    Home page
    modified code

--- BayBoon/src/main/java/pom/HomePage.java
+++ BayBoon/src/main/java/pom/HomePage.java
@@ -1,5 +1,16 @@
 package pom;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
 
+public class HomePage  {
+	static WebDriver driver;
+	public void test(){
+		ConsumerSignin cs = new ConsumerSignin();
+		cs.signIn();
+	}
+	public static void main(String[] args){
+		HomePage home = new HomePage();
+		home.test();
+	}
+	
 }
