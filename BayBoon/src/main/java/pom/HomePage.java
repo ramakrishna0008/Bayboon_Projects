f397524e6051d6b1b22ced1d9dac5b20a032ca46
ramakrishna0008 <ramakrishna0008@gmail.com>
2017-05-03 00:19:36 +0530

    code modified in Home page

--- BayBoon/src/main/java/pom/HomePage.java
+++ BayBoon/src/main/java/pom/HomePage.java
@@ -2,13 +2,14 @@ package pom;
 
 import org.openqa.selenium.WebDriver;
 
-public class HomePage  {
-	static WebDriver driver;
+public class HomePage extends TestConsumerSignin  {
+	//static WebDriver driver;
 	
 	public void test(){
 		//ConsumerSignin cs = new ConsumerSignin();
-		TestConsumerSignin tcs = new TestConsumerSignin();
-		tcs.signIn();
+		//TestConsumerSignin tcs = new TestConsumerSignin();
+		//tcs.signIn();
+		signIn();
 	}
 	public static void main(String[] args){
 		HomePage home = new HomePage();
